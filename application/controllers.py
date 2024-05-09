ebdd8c5033eaa4bb1d319971bd3f2d0b5325f410
ym496 <yogendramishra2004@gmail.com>
2024-05-09 12:20:35 +0530

    add login logout func

--- application/controllers.py
+++ application/controllers.py
@@ -1,9 +1,51 @@
-from flask import Flask, request, render_template, redirect, url_for, jsonify
+from flask import Flask, request, render_template, redirect, url_for, jsonify, session
 from flask import current_app as app
-from application.models import Runner, db
+from flask_login import LoginManager, login_user, logout_user, current_user
+from application.models import User, Admin, Runner, db
 
-@app.route('/',methods=["GET","POST"])
+app.secret_key = '1234'
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        return user
+
+    admin = Admin.query.get(user_id)
+    if admin:
+        return admin
+
+    return None
+
+
+@app.route('/')
 def index():
-    
-    if request.method == "GET":
-        return render_template('index.html')
+    if session.get('role') == 'user' or session.get('role') == 'admin':
+        return redirect(url_for('monitor'))
+
+    return render_template('index.html')
+
+@app.post('/login')
+def login():
+    email = request.form.get('email')
+    print(email)
+    password = request.form.get('password')
+    user = User.query.filter_by(email=email).first()
+    if user and user.password == password:
+        login_user(user)
+        session['role'] = "user"
+        return redirect(url_for('monitor'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    session.pop('role', None)
+    return redirect(url_for('index'))
+
+
+@app.route('/monitor')
+def monitor():
+    if session.get('role') == 'user' or session.get('role') == 'admin':
+        return render_template('monitor.html')
+    return redirect(url_for('index'))
--- application/models.py
+++ application/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 db = SQLAlchemy()
 
@@ -8,3 +9,13 @@ class Runner(db.Model):
     bib_code = db.Column(db.String(20), unique=True, nullable=False)
     start_time = db.Column(db.DateTime)
     finish_time = db.Column(db.DateTime)
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+class Admin(db.Model, UserMixin):
+    id = db.Column(db.String(50), primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
